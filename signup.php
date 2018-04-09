4ec133bb59e33f185ac939a758d539924112df7e
shaifat <skshaifatmurshed@gmail.com>
2018-04-09 11:41:14 +0530

    added new form

---
+++ signup.php
@@ -0,0 +1,91 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Sign Up</title>
+		<meta charset="utf-8">
+	</head>
+	<body class="subpage">
+
+						<header>
+							<h2 align="center" style="color:green"><mark><i>Create a new account </i></mark></h2>
+							
+						</header>
+						<form method="post" action=" " enctype="multipart/form-data">
+<table align="center" style="background-color:#666">
+<tr>
+	<td><b>First Name</b>*</td>
+	<td><input type='text' name="fname" placeholder='Enter your first name...' </td><br>
+</tr>
+<tr>
+<tr>
+	<td><b>Middle Name</b></td>
+	<td><input type='text' name="mname" placeholder='Enter your middle name...' </td><br>
+</tr>
+<tr>
+	<td><b>last Name</b>*</td>
+	<td><input type='text' name="lname" placeholder='Enter your last name...' </td><br>
+</tr>
+<tr>
+	<td><b>Email</b>*</td>
+	<td><input type='email' name="email" placeholder='Enter your Email Id...' </td><br>
+</tr>
+<tr>
+	<td><b>Password</b>*</td>
+	<td><input type="password" name="password" placeholder="Enter 6 character password"><br></td>
+</tr>
+<tr>
+	<td><b>Phone no.</b>*</td>
+	<td><input type="text" name="phone" placeholder="Enter your mobile number"></td>
+</tr>
+<tr>
+	<td><b>Gender</b>*</td>
+	<td><select name="gender">
+    <option value="">-- select one --</option>
+  <option value="Male" style="color:black">Male</option>
+  <option value="Female" style="color:black">Female</option>
+  <option value="other" style="color:black">other</option>
+  </option>
+ </select></td>
+</tr>
+<tr>
+	<td><b>Date of birth*</b></td>
+	<td><input type="date" name="dob"></td>
+</tr>
+<tr>
+	<td><b>About</b>*</td>
+	<td><input type='text' name="about" placeholder='write something ...' </td><br>
+</tr>
+<tr>
+	<td><b>Occupation</b></td>
+	<td><input type="text" name="occupation"></td>
+</tr>
+<tr>
+	<td><b>Address</b>*</td>
+	<td><textarea name="address" name="address" placeholder="Enter your address"></textarea></td><br>
+</tr>
+<tr>
+	<td><b>Choose a pic</b>*</td>
+	<td><input type="file" name="file"></td>
+</tr>
+<tr>
+	<td><b>Security question</b>*</td>
+    <td><select name="securityquestion">
+    <option value="">-- select one --</option>
+  <option value="nickname" style="color:black">My nick name</option>
+  <option value="petname" style="color:black">My pet name</option>
+  <option value="hobby" style="color:black">My hobby</option>
+  </option>
+ </select></td>
+	</tr>
+<tr>
+	<td><b>Security Answer</b>*</td>
+	<td><input type='text' name="securityanswer" placeholder='Enter your Answer...' ></td><br>
+</tr>
+<tr>
+	<td><input type='submit' name='ok' value='Sign up'></td>
+</tr>
+
+</table>
+</form>
+<p align="center" style="color:red"><b>N/A- "The * mark area should be fulfilled by new user"</b></p><br>
